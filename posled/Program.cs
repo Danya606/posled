cf06075585f89126108e6c299e6c8f62a8a6897f
Danya606 <danya606spb@yandex.ru>
2024-02-27 09:35:45 +0300

    Hyyy

--- posled/Program.cs
+++ posled/Program.cs
@@ -7,6 +7,6 @@ class Program
         Console.Write("Введите целое число x: ");
         int x = Convert.ToInt32(Console.ReadLine());
         int y = 5 * x + 20;
-        Console.WriteLine($"Результат: {y}");
+        Console.WriteLine($"Результат: {y-2}");
     }
 }
